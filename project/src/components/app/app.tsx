5d03f318ebbde91846a8cf04e7ebc5b9faea08b7
AlexandraNedosekina <a.nedosekina@okenit.ru>
2022-10-29 16:38:04 +0500

    3.2. Добавит компоненты страниц и отрисовку в app.tsx

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,14 +1,49 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import WelcomeFilm from '../../pages/welcome-movie-page/welcome-movie-page';
+import MainPage from '../../pages/main-page/main-page';
+import {AppRoute} from '../../const';
+import SignInPage from '../../pages/sign-in-page/sign-in-page';
+import FilmCard from '../../pages/film-card-page/film-card-page';
+import AddReview from '../../pages/add-review-page/add-review-page';
+import MyList from '../../pages/my-list-page/my-list-page';
+import PlayerPage from '../../pages/player-page/player-page';
 
-
-type AppScreenProps = {
-  errorsCount: number;
+type mainFilmProps = {
+  title: string,
+  genre: string,
+  year: number
 }
 
-function App({errorsCount}:AppScreenProps): JSX.Element {
+function App(MainScreenProps : mainFilmProps): JSX.Element {
   return (
-    <WelcomeFilm errorsCount={errorsCount}/>
+    <BrowserRouter>
+
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage mainFilmProps={MainScreenProps}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<SignInPage />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmCard/>}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReview/>}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyList/>}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
