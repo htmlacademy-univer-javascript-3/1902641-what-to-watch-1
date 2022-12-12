83284433e9930a4eaca642878983a043dc4bbaa8
AlexandraNedosekina <a.nedosekina@okenit.ru>
2022-12-12 18:01:48 +0500

    5.1. Создаст действия и редьюсер

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,18 +6,22 @@ import COMMENTS from './mocks/comments';
 import SIMILAR from './mocks/similar';
 import FAVORITE from './mocks/favorite';
 import FILMS from './mocks/films';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App
-      promo={promo}
-      films={FILMS}
-      reviews={COMMENTS}
-      similar={SIMILAR}
-      favorite={FAVORITE}
-    />
+    <Provider store={store}>
+      <App
+        promo={promo}
+        films={FILMS}
+        reviews={COMMENTS}
+        similar={SIMILAR}
+        favorite={FAVORITE}
+      />
+    </Provider>
   </React.StrictMode>,
 );
