b5fa0de7666dc98fac4272854b932bec1884b805
AlexandraNedosekina <a.nedosekina@okenit.ru>
2022-12-14 23:54:46 +0500

    7.1. Отключит тестовые данные

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,6 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
-import films from '../mocks/films';
-import { DEFAULT_GENRE, FilmPageTabs } from '../const';
+import { AuthorizationStatus, DEFAULT_GENRE, FilmPageTabs } from '../const';
 import {
   changeFilmTab,
   changeGenre,
@@ -9,23 +8,38 @@ import {
   resetFilmScreen,
   resetMainScreen
 } from './action';
-import {filterFilmsByGenre} from '../utils/get-film';
+import { filterFilmsByGenre } from '../utils/get-film';
+import Films from '../types/films';
 
-const initialState = {
-  films: films,
+type InitialState = {
+  films: Films;
+  currentGenre: string,
+  filteredFilms: Films,
+  cardCount: number,
+  authorizationStatus: string,
+  filmPageTab: string
+}
+
+const initialState: InitialState = {
+  films: [],
+
   currentGenre: DEFAULT_GENRE,
-  filteredFilms: films,
-  cardCount: films.length < 8 ? films.length : 8,
+  filteredFilms: [],
+  cardCount: 0,
+
+  authorizationStatus: AuthorizationStatus.Unknown,
+
   filmPageTab: FilmPageTabs.Overview as string,
 };
-
 export const reducer = createReducer(initialState, (builder) => {
   builder
+
     .addCase(resetMainScreen, (state) => {
       state.currentGenre = DEFAULT_GENRE;
-      state.filteredFilms = films;
-      state.cardCount = films.length < 8 ? films.length : 8;
+      state.filteredFilms = state.films;
+      state.cardCount = state.films.length < 8 ? state.films.length : 8;
     })
+
     .addCase(changeGenre, (state, action) => {
       const filteredFilms = filterFilmsByGenre(state.films, action.payload.currentGenre);
       state.currentGenre = action.payload.currentGenre;
@@ -34,11 +48,13 @@ export const reducer = createReducer(initialState, (builder) => {
         filteredFilms.length :
         8;
     })
+
     .addCase(increaseCardCount, (state) => {
       state.cardCount = (state.cardCount + 8) < state.filteredFilms.length ?
         state.cardCount + 8 :
         state.filteredFilms.length;
     })
+
     .addCase(resetCardCount, (state) => {
       state.cardCount = state.filteredFilms.length < 8 ?
         state.filteredFilms.length :
@@ -48,7 +64,15 @@ export const reducer = createReducer(initialState, (builder) => {
     .addCase(resetFilmScreen, (state) => {
       state.filmPageTab = FilmPageTabs.Overview;
     })
+
     .addCase(changeFilmTab, (state, action) => {
       state.filmPageTab = action.payload.currentTab;
     });
+
+    .addCase(loadFilms, (state, action) => {
+      state.films = action.payload;
+    })
+
+  .addCase(requireAuthorization, (state, action) => {
+    state.authorizationStatus = action.payload;
 });
